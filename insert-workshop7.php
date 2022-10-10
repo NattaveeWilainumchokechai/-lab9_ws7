f793f65424e4cd531599ca833408b43f6fb7e007
NattaveeWilainumchokechai <92027360+NattaveeWilainumchokechai@users.noreply.github.com>
2022-10-10 13:29:58 +0700

    Add files via upload

---
+++ insert-workshop7.php
@@ -0,0 +1,22 @@
+<?php
+    $pdo = new PDO("mysql:host=localhost;dbname=blueshop;charset=utf8", "root", "");
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+?>
+
+<?php
+    $stmt = $pdo->prepare("INSERT INTO member VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bindParam(1, $_POST["usn"]);
+    $stmt->bindParam(2, $_POST["pwd"]);
+    $stmt->bindParam(3, $_POST["name"]);
+    $stmt->bindParam(4, $_POST["address"]);
+    $stmt->bindParam(5, $_POST["tel"]);
+    $stmt->bindParam(6, $_POST["mail"]);
+    $stmt->execute();
+?>
+
+<html>
+    <head><meta charset="UTF-8"></head>
+    <body>
+        เพิ่มข้อมูลแล้ว
+    </body>
+</html>
